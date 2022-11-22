c0bbecb6e3b4719766a3927bf358db4ee86ce929
lecomte38 <nicolas.lecomte@etu.unilim.fr>
2022-11-22 16:28:46 +0000

    main: tables SQL creation

--- tables.sql
+++ tables.sql
@@ -1,13 +1,147 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost
+-- Généré le : mar. 22 nov. 2022 à 16:26
+-- Version du serveur : 8.0.26-0ubuntu0.20.04.2
+-- Version de PHP : 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `carpooling`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `carpool_ads`
+--
+
+CREATE TABLE `carpool_ads` (
+  `id` int NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `idCar` int NOT NULL,
+  `idAdvertiser` int NOT NULL,
+  `departurePlace` varchar(255) NOT NULL,
+  `arrivalPlace` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `cars`
+--
+
+CREATE TABLE `cars` (
+  `id` int NOT NULL,
+  `brand` varchar(255) NOT NULL,
+  `model` varchar(255) NOT NULL,
+  `nbSeat` int NOT NULL,
+  `idOwner` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `reservations`
+--
+
+CREATE TABLE `reservations` (
+  `id` int NOT NULL,
+  `idCarpoolAd` int NOT NULL,
+  `idClient` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
 CREATE TABLE `users` (
-  `id` int AUTO_INCREMENT NOT NULL,
+  `id` int NOT NULL,
   `firstname` varchar(255) NOT NULL,
   `lastname` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `birthday` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `birthday` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+--
+-- Déchargement des données de la table `users`
+--
 
 INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `birthday`) VALUES
-(1, 'Vincent', 'Godé', 'hello@vincentgo.fr', '1990-11-08'),
-(2, 'Albert', 'Dupond', 'sonemail@gmail.com', '1985-11-08'),
-(3, 'Thomas', 'Dumoulin', 'sonemail2@gmail.com', '1985-10-08');
+(1, 'Vincent', 'Godé', 'hello@vincentgo.fr', '1990-11-08 00:00:00'),
+(2, 'Albert', 'Dupond', 'sonemail@gmail.com', '1985-11-08 00:00:00'),
+(3, 'Thomas', 'Dumoulin', 'sonemail2@gmail.com', '1985-10-08 00:00:00');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `carpool_ads`
+--
+ALTER TABLE `carpool_ads`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `cars`
+--
+ALTER TABLE `cars`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `reservations`
+--
+ALTER TABLE `reservations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `carpool_ads`
+--
+ALTER TABLE `carpool_ads`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `cars`
+--
+ALTER TABLE `cars`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `reservations`
+--
+ALTER TABLE `reservations`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
